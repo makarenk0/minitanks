ac7e8b0d751ecd42f4263da4287f6127aaafa30e
LordNani <rigansu@gmail.com>
2019-06-07 17:21:30 +0300

    testCommit

--- minitanks/Renderer.cpp
+++
@@ -1,7 +0,0 @@
-#include "Renderer.h"
-#include <iostream>
-
-
-void Render(sf::RenderWindow &mWindow, std::vector<sf::Sprite> &vecSprite, sf::Sprite& p1){
-
-}
--- minitanks/Renderer.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-
-void Render(sf::RenderWindow& mWindow, std::vector < sf::Sprite> &vecSprite, sf::Sprite& p1);
