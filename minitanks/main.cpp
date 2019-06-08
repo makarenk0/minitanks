7ee59bd355968b4becb38bd17bf3df14db577c14
makarenk0 <45842815+makarenk0@users.noreply.github.com>
2019-06-08 16:31:59 +0300

    Merged files(not work)

--- minitanks/main.cpp
+++ minitanks/main.cpp
@@ -22,10 +22,10 @@ int main() {
   sf::RenderWindow mWindow(sf::VideoMode(mapWidth + 2 * widgetWidth, mapHeight),
                            "Main window",
                            sf::Style::Close | sf::Style::Titlebar);
-
+  mWindow.setFramerateLimit(FPS);
   TileMap map("example_map", cellWidth, mapWidth, mapHeight, false, false);
-  Player pl("Player.png", 20, 20, playerSize, playerSize, 2, 1, cellWidth,
-            &map);
+ Player pl("Player.png", 20, 20, playerSize, playerSize, 2, 1, cellWidth,
+           &map);
 
   Widget p1Widget(sf::Vector2f(0, 0), widgetWidth, mapHeight, true);
   Widget p2Widget(sf::Vector2f(mapWidth + widgetWidth, 0), widgetWidth,
@@ -90,7 +90,7 @@ int main() {
 			  map.setExitEditMode(true);
 		  }
 	  }
-
+	  
 	  // for map
 	  mWindow.clear();
 	  pl.updatePlayer();
